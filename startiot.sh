e9e3eb1ae541c85cbac44257a6b6cffc2823456b
Dave Glover <dglover@microsoft.com>
2016-10-03 10:28:05 +1100

    initial checkin

---
+++ startiot.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "starting IoT Hub Client"
+
+sleep 10
+
+sudo killall python3
+
+cd /home/pi/iothub/weather_mqtt
+
+python3 weather_mqtt.py config_openweather.json&
+#python3 weather_mqtt.py config_envirophat.json&
+#python3 weather_mqtt.py config_sensehat.json&
+
